6b79db7bed23ac5a522d5b03b4809b075a167cf8
chrisgigantic <christopherraegigantana@gmail.com>
2018-05-08 13:45:20 +0800

    Create 4-sec-timer.vhdl

---
+++ 4-sec-timer.vhdl
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity four_second_timer is
+	port (
+		a: in std_ulogic;
+		b: out std_ulogic;
+	);
+	
+end entity four_second_timer;
+
+architecture count of four_second_timer is
+
+begin
+	
+end architecture count;
